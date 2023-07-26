d929ff9d90e66ce45dee6e3d5a38bc5dc04ea4c2
Your Name <varlamoved@gmail.com>
2023-07-26 16:44:46 +0300

    add enemies

---
+++ src/Entity.hx
@@ -0,0 +1,41 @@
+import h2d.Scene;
+
+class Entity {
+    var x : Int;
+    var y : Int;
+
+    var size : Int;
+
+    var scene : Scene;
+
+    var bmp : h2d.Bitmap;
+    var speed = 5;
+
+   
+
+    public function new(x, y) {
+
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public function init(scene: Scene) {
+      this.scene = scene;
+
+      var tile = h2d.Tile.fromColor(0xFFFFFF, 100, 100);
+      tile.dx = -tile.width * 0.5;
+      tile.dy = -tile.height * 0.5;
+      
+     //create a bitmap jbject and add it to the default 2d scene (s2d)
+      bmp = new h2d.Bitmap(tile, scene);
+    }
+
+    public function update(dt:Float) {
+
+       x++;
+      bmp.x = x; 
+
+    }
+
+}
--- src/Main.hx
+++ src/Main.hx
@@ -1,15 +1,19 @@
 import haxe.io.Float32Array;
 import hxd.FloatBuffer;
 
+using Lambda;
+
 class Main extends hxd.App{
     var bmp : h2d.Bitmap;
 
     var velocity : Float = 0;
     var gravity : Float = 10;
     
-    var chunkSize : Float = 50;
+    var chunkSize : Float = 100;
 
-    override function init () {
+    var enemies : Array<Enemy>;
+
+    override function init (){
        // create a white box + create 100x100 tile from it
         var tile = h2d.Tile.fromColor(0xFFFFFF, 100, 100);
         tile.dx = -tile.width * 0.5;
@@ -18,6 +22,11 @@ class Main extends hxd.App{
        //create a bitmap jbject and add it to the default 2d scene (s2d)
         bmp = new h2d.Bitmap(tile, s2d);
 
+        var enemy = new Enemy(100, 100);
+        enemies.push(enemy);
+       // enemies.iter(function(enemy) enemy.init(s2d));
+
+
        //modify the display position of bitmap
        bmp.x = s2d.width * 0.5;
        bmp.y = s2d.height * 0.5;
@@ -25,6 +34,8 @@ class Main extends hxd.App{
 
     override function update(dt:Float) {
         
+       // recalculate_enemies(dt);
+
         bmp.y += velocity * chunkSize * dt;
         velocity += gravity * dt;
 
@@ -33,6 +44,10 @@ class Main extends hxd.App{
         }
 
     
+    }
+
+    function recalculate_enemies(dt:Float) {
+        enemies.iter(function(enemy) enemy.update(dt));
     }
 
     static function main() {
