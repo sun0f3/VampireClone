70e4f29cb019ebb78b46354e2ea40bf4f8e0cfe3
Your Name <varlamoved@gmail.com>
2023-08-05 11:06:48 +0300

    add Config

---
+++ main/config.lua
@@ -0,0 +1,20 @@
+
+
+local Config = {
+	hero ={
+		speed = 5,
+		hp = 10
+	},
+	weapons = {
+		rifle = { distance = 10, type = "point", damage = 20, delay = 4 },
+		pistol = { distance = 5, type = "point", damage = 5, delay = 1 }		
+	},
+	enemies = {
+		rabbit =  { hp = 100, speep = 2},
+	},
+
+	abills = {
+	}
+}
+
+return Config
