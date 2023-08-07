51f75bf1fe4047c3c60660721b679564e3319578
Your Name <varlamoved@gmail.com>
2023-08-07 19:16:21 +0300

    add audio to game

--- main/enemy.go
+++ main/enemy.go
@@ -91,3 +91,49 @@ embedded_components {
     z: 1.0
   }
 }
+embedded_components {
+  id: "hit"
+  type: "sound"
+  data: "sound: \"/audio/SoundEffect/hit.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "death"
+  type: "sound"
+  data: "sound: \"/audio/SoundEffect/death.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
