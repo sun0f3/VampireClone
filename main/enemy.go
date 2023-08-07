31c38c2e78e4b71259513e3af88b26d7fbffb783
Your Name <varlamoved@gmail.com>
2023-08-07 20:19:45 +0300

    add factory

--- main/enemy.go
+++ main/enemy.go
@@ -24,18 +24,13 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
-    z: 0.0
+    z: 1.0
   }
   rotation {
     x: 0.0
     y: 0.0
     z: 0.0
     w: 1.0
-  }
-  scale {
-    x: 0.005
-    y: 0.005
-    z: 1.0
   }
 }
 embedded_components {
@@ -124,6 +119,54 @@ embedded_components {
   "pan: 0.0\n"
   "speed: 1.0\n"
   "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: -85.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 50.0\n"
+  "  data: 10.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: true\n"
+  "bullet: false\n"
   ""
   position {
     x: 0.0
